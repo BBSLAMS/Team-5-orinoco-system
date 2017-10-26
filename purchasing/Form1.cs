f6a73f249b474a1d9168c922f874cf34e5963fa0
Wieun <32953853+Wieun@users.noreply.github.com>
2017-10-26 11:49:30 +0100

    started working on the form
    ive done the first form where the user inputs the data, however the confrim button dosent parse the values to the new form

---
+++ purchasing/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace purchasing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonAddItem_Click(object sender, EventArgs e)
+        {
+            listBoxItems.Items.Add(comboBoxItems.SelectedItem);
+        }
+
+        private void buttonRemove_Click(object sender, EventArgs e)
+        {
+            listBoxItems.Items.Remove(listBoxItems.SelectedItem);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonRequestNewItem_Click(object sender, EventArgs e)
+        {
+            textBoxNewItemRequest.Visible = true;
+            textBoxReasonforRequest.Visible = true;
+            label3.Visible = true;
+            label4.Visible = true;
+        }
+
+        private void buttonConfirm_Click(object sender, EventArgs e)
+        {
+            /* string departmentName = "IT";
+             confirm frm2 = new confirm();
+            frm2.checkedListBox1.Items = listBoxItems.Items;
+             frm2.Show();
+             this.Hide();*/
+            confirm frm2 = new confirm();
+            frm2.checkedListBox1.Items.Add(listBoxItems.Text);
+
+
+            frm2.Show();
+        }
+
+        private void listBoxItems_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ purchasing/confirm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace purchasing
+{
+    public partial class confirm : Form
+    {
+        public confirm()
+        {
+            InitializeComponent();
+        }
+
+        private void confirm_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        public void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Form1 frm1 = new Form1();
+            checkedListBox1.Items.Add(frm1.listBoxItems);
+        }
+
+        
+    }
+}
