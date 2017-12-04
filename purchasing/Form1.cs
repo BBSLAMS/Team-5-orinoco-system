3c834664b29ffd4115d84f8f221e34ef09204ad5
Connor Scourfield <scourf95@gmail.com>
2017-12-04 12:51:31 +0000

    Clone from Eric's branch

---
+++ purchasing/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace purchasing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonAddItem_Click(object sender, EventArgs e)
+        {
+            listBoxItems.Items.Add(comboBoxItems.SelectedItem);
+            listBoxItems.Items.Add(nudNoOfOrders.Value);
+           
+        }
+
+        private void buttonRemove_Click(object sender, EventArgs e)
+        {
+            listBoxItems.Items.Remove(listBoxItems.SelectedItem);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonRequestNewItem_Click(object sender, EventArgs e)
+        {
+            textBoxNewItemRequest.Visible = true;
+            textBoxReasonforRequest.Visible = true;
+            label3.Visible = true;
+            label4.Visible = true;
+        }
+
+        private void buttonConfirm_Click(object sender, EventArgs e)
+        {
+            confirm frm2 = new confirm();
+            frm2.checkedListBox1.Items.AddRange(listBoxItems.Items);
+            frm2.Show();
+            TextWriter txt = new StreamWriter("C:\\Users\\ericc\\Documents\\Github\\test.txt");
+            txt.Write(listBoxItems.Items);
+            txt.Close();
+        }
+
+        private void listBoxItems_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nudNoOfOrders_ValueChanged(object sender, EventArgs e)
+        {
+            nudNoOfOrders.Minimum = 0;
+            nudNoOfOrders.ThousandsSeparator = true;
+        }
+    }
+}
---
+++ purchasing/confirm.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace purchasing
+{
+    partial class confirm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.buttonConfirm = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(62, 68);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(208, 89);
+            this.checkedListBox1.TabIndex = 0;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // buttonConfirm
+            // 
+            this.buttonConfirm.Location = new System.Drawing.Point(62, 163);
+            this.buttonConfirm.Name = "buttonConfirm";
+            this.buttonConfirm.Size = new System.Drawing.Size(75, 23);
+            this.buttonConfirm.TabIndex = 1;
+            this.buttonConfirm.Text = "confirm";
+            this.buttonConfirm.UseVisualStyleBackColor = true;
+            this.buttonConfirm.Click += new System.EventHandler(this.buttonConfirm_Click);
+            // 
+            // confirm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.buttonConfirm);
+            this.Controls.Add(this.checkedListBox1);
+            this.Name = "confirm";
+            this.Text = "confirm";
+            this.Load += new System.EventHandler(this.confirm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button buttonConfirm;
+    }
+}
